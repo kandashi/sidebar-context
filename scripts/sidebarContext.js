200e0909a3cbeec0c0a3ce8f673354120176bf7e
kandashi <jacobmpalmer@live.co.uk>
2022-02-03 01:34:25 +0000

    PR fixes

--- scripts/sidebarContext.js
+++ scripts/sidebarContext.js
@@ -1,135 +1,135 @@
 RollTableDirectory.prototype._getEntryContextOptions = function newTableContext() {
-    const options = SidebarDirectory.prototype._getEntryContextOptions.call(this);
-    return [
-        {
-            name: "TABLE.Roll",
-            icon: `<i class="fas fa-dice-d20"></i>`,
-            condition: li => {
-                const table = game.tables.get(li.data("documentId"));
-                return table.data.img !== CONST.DEFAULT_TOKEN;
-            },
-            callback: li => {
-                const table = game.tables.get(li.data("documentId"));
-                table.draw()
-            }
-        }
-    ].concat(options);
+  const options = SidebarDirectory.prototype._getEntryContextOptions.call(this);
+  return [
+    {
+      name: "TABLE.Roll",
+      icon: `<i class="fas fa-dice-d20"></i>`,
+      condition: li => {
+        const table = game.tables.get(li.data("documentId"));
+        return table.data.img !== CONST.DEFAULT_TOKEN;
+      },
+      callback: li => {
+        const table = game.tables.get(li.data("documentId"));
+        table.draw()
+      }
+    }
+  ].concat(options);
 }
 
 ActorDirectory.prototype._getEntryContextOptions = function newActorContext() {
-    const options = SidebarDirectory.prototype._getEntryContextOptions.call(this);
-    return [
-        {
-            name: "SIDEBAR.CharArt",
-            icon: '<i class="fas fa-image"></i>',
-            condition: li => {
-                const actor = game.actors.get(li.data("documentId"));
-                if(game.user.isGM || (actor.owner && game.user.can("TOKEN_CONFIGURE"))){
-                  return actor.data.img !== CONST.DEFAULT_TOKEN;
-                }else{
-                  return false;
-                }
-            },
-            callback: li => {
-                const actor = game.actors.get(li.data("documentId"));
-                new ImagePopout(actor.data.img, {
-                    title: actor.name,
-                    shareable: true,
-                    uuid: actor.uuid
-                }).render(true);
-            }
-        },
-        {
-            name: "SIDEBAR.TokenArt",
-            icon: '<i class="fas fa-image"></i>',
-            condition: li => {
-                const actor = game.actors.get(li.data("documentId"));
-                if(game.user.isGM || (actor.owner && game.user.can("TOKEN_CONFIGURE"))){
-                  if (actor.data.token.randomImg) return false;
-                  return ![null, undefined, CONST.DEFAULT_TOKEN].includes(actor.data.token.img);
-                }else{
-                  return false;
-                }
-            },
-            callback: li => {
-                const actor = game.actors.get(li.data("documentId"));
-                new ImagePopout(actor.data.token.img, {
-                    title: actor.name,
-                    shareable: true,
-                    uuid: actor.uuid
-                }).render(true);
-            }
-        },
-        {
-            name: "sidebar-context.prototype",
-            icon: '<i class="fas fa-user circle"></i>',
-            condition: li => {
-                const actor = game.actors.get(li.data("documentId"));
-                if(game.user.isGM || (actor.owner && game.user.can("TOKEN_CONFIGURE"))){
-                  return true;
-                }else{
-                  return false;
-                }
-            },
-            callback: li => {
-                const actor = game.actors.get(li.data("documentId"));
-                new CONFIG.Token.prototypeSheetClass(actor, {
-                    left: Math.max(this.position.left - 560 - 10, 10),
-                    top: this.position.top
-                }).render(true);
-            }
-        },
-        {
-            name: "sidebar-context.updateChildren",
-            icon: `<i class="fas fa-user-edit"></i>`,
-            condition: li => {
-                const actor = game.actors.get(li.data("documentId"));
-                if(game.user.isGM || (actor.owner && game.user.can("TOKEN_CONFIGURE"))){
-                  return !actor.data.token.actorLink;
-                }else{
-                  return false;
-                }
-            },
-            callback: li => {
-                const actor = game.actors.get(li.data("documentId"));
-                updateChildren.call(actor)
-            }
+  const options = SidebarDirectory.prototype._getEntryContextOptions.call(this);
+  return [
+    {
+      name: "SIDEBAR.CharArt",
+      icon: '<i class="fas fa-image"></i>',
+      condition: li => {
+        const actor = game.actors.get(li.data("documentId"));
+        if (game.user.isGM || (actor.owner && game.user.can("TOKEN_CONFIGURE"))) {
+          return actor.data.img !== CONST.DEFAULT_TOKEN;
+        } else {
+          return false;
         }
-    ].concat(options);
+      },
+      callback: li => {
+        const actor = game.actors.get(li.data("documentId"));
+        new ImagePopout(actor.data.img, {
+          title: actor.name,
+          shareable: true,
+          uuid: actor.uuid
+        }).render(true);
+      }
+    },
+    {
+      name: "SIDEBAR.TokenArt",
+      icon: '<i class="fas fa-image"></i>',
+      condition: li => {
+        const actor = game.actors.get(li.data("documentId"));
+        if (game.user.isGM || (actor.owner && game.user.can("TOKEN_CONFIGURE"))) {
+          if (actor.data.token.randomImg) return false;
+          return ![null, undefined, CONST.DEFAULT_TOKEN].includes(actor.data.token.img);
+        } else {
+          return false;
+        }
+      },
+      callback: li => {
+        const actor = game.actors.get(li.data("documentId"));
+        new ImagePopout(actor.data.token.img, {
+          title: actor.name,
+          shareable: true,
+          uuid: actor.uuid
+        }).render(true);
+      }
+    },
+    {
+      name: "sidebar-context.prototype",
+      icon: '<i class="fas fa-user circle"></i>',
+      condition: li => {
+        const actor = game.actors.get(li.data("documentId"));
+        if (game.user.isGM || (actor.owner && game.user.can("TOKEN_CONFIGURE"))) {
+          return true;
+        } else {
+          return false;
+        }
+      },
+      callback: li => {
+        const actor = game.actors.get(li.data("documentId"));
+        new CONFIG.Token.prototypeSheetClass(actor, {
+          left: Math.max(this.position.left - 560 - 10, 10),
+          top: this.position.top
+        }).render(true);
+      }
+    },
+    {
+      name: "sidebar-context.updateChildren",
+      icon: `<i class="fas fa-user-edit"></i>`,
+      condition: li => {
+        const actor = game.actors.get(li.data("documentId"));
+        if (game.user.isGM || (actor.owner && game.user.can("TOKEN_CONFIGURE"))) {
+          return !actor.data.token.actorLink;
+        } else {
+          return false;
+        }
+      },
+      callback: li => {
+        const actor = game.actors.get(li.data("documentId"));
+        updateChildren.call(actor)
+      }
+    }
+  ].concat(options);
 }
 
 
 ItemDirectory.prototype._getEntryContextOptions = function newItemContext() {
-    const options = SidebarDirectory.prototype._getEntryContextOptions.call(this);
-    return [
-        {
-            name: "ITEM.ViewArt",
-            icon: '<i class="fas fa-image"></i>',
-            condition: li => {
-                const item = game.items.get(li.data("documentId"));
-                return item.data.img !== CONST.DEFAULT_TOKEN;
-            },
-            callback: li => {
-                const item = game.items.get(li.data("documentId"));
-                new ImagePopout(item.data.img, {
-                    title: item.name,
-                    shareable: true,
-                    uuid: item.uuid
-                }).render(true);
-            }
-        },
-        {
-            name: "sidebar-context.displayChat",
-            icon: `<i class="fas fa-dice-d20"></i>`,
-            condition: li => {
-                return true
-            },
-            callback: li => {
-                const item = game.items.get(li.data("documentId"));
-                newChatCard.call(item)
-            }
-        },
-    ].concat(options);
+  const options = SidebarDirectory.prototype._getEntryContextOptions.call(this);
+  return [
+    {
+      name: "ITEM.ViewArt",
+      icon: '<i class="fas fa-image"></i>',
+      condition: li => {
+        const item = game.items.get(li.data("documentId"));
+        return item.data.img !== CONST.DEFAULT_TOKEN;
+      },
+      callback: li => {
+        const item = game.items.get(li.data("documentId"));
+        new ImagePopout(item.data.img, {
+          title: item.name,
+          shareable: true,
+          uuid: item.uuid
+        }).render(true);
+      }
+    },
+    {
+      name: "sidebar-context.displayChat",
+      icon: `<i class="fas fa-dice-d20"></i>`,
+      condition: li => {
+        return true
+      },
+      callback: li => {
+        const item = game.items.get(li.data("documentId"));
+        newChatCard.call(item)
+      }
+    },
+  ].concat(options);
 }
 
 SceneDirectory.prototype._getEntryContextOptions = function newSceneEntryContext() {
@@ -162,7 +162,7 @@ SceneDirectory.prototype._getEntryContextOptions = function newSceneEntryContext
   ].concat(options);
 }
 
-SceneDirectory.prototype._getFolderContextOptions  = function newSceneFolderContext() {
+SceneDirectory.prototype._getFolderContextOptions = function newSceneFolderContext() {
   const options = SidebarDirectory.prototype._getFolderContextOptions.call(this);
   return [
     {
@@ -172,8 +172,8 @@ SceneDirectory.prototype._getFolderContextOptions  = function newSceneFolderCont
         return game.user?.isGM;
       },
       callback: (header) => {
-          const folderId = header.parent().data('folderId');
-          setNavigationForAllScenes(folderId, true);
+        const folderId = header.parent().data('folderId');
+        setNavigationForAllScenes(folderId, true);
       }
     },
     {
@@ -183,77 +183,80 @@ SceneDirectory.prototype._getFolderContextOptions  = function newSceneFolderCont
         return game.user?.isGM;
       },
       callback: (header) => {
-          const folderId = header.parent().data('folderId');
-          setNavigationForAllScenes(folderId, false);
+        const folderId = header.parent().data('folderId');
+        setNavigationForAllScenes(folderId, false);
       }
     }
   ].concat(options);
 }
 
 async function newChatCard() {
-    const templateData = {
-        item: this.data,
-        data: this.getChatData(),
-        labels: this.labels,
-        hasAttack: this.hasAttack,
-        isHealing: this.isHealing,
-        hasDamage: this.hasDamage,
-        isVersatile: this.isVersatile,
-        isSpell: this.data.type === "spell",
-        hasSave: this.hasSave,
-        hasAreaTarget: this.hasAreaTarget,
-        isTool: this.data.type === "tool",
-        hasAbilityCheck: this.hasAbilityCheck
-    };
-    const html = await renderTemplate("systems/dnd5e/templates/chat/item-card.html", templateData);
+  const templateData = {
+    item: this.data,
+    data: this.getChatData(),
+    labels: this.labels,
+    hasAttack: this.hasAttack,
+    isHealing: this.isHealing,
+    hasDamage: this.hasDamage,
+    isVersatile: this.isVersatile,
+    isSpell: this.data.type === "spell",
+    hasSave: this.hasSave,
+    hasAreaTarget: this.hasAreaTarget,
+    isTool: this.data.type === "tool",
+    hasAbilityCheck: this.hasAbilityCheck
+  };
+  const html = await renderTemplate("systems/dnd5e/templates/chat/item-card.html", templateData);
 
-    // Create the ChatMessage data object
-    const chatData = {
-        user: game.user.data._id,
-        type: CONST.CHAT_MESSAGE_TYPES.OTHER,
-        content: html,
-        flavor: this.data.data.chatFlavor || this.name,
-        flags: { "core.canPopout": true }
-    };
+  // Create the ChatMessage data object
+  const chatData = {
+    user: game.user.data._id,
+    type: CONST.CHAT_MESSAGE_TYPES.OTHER,
+    content: html,
+    flavor: this.data.data.chatFlavor || this.name,
+    flags: { "core.canPopout": true }
+  };
 
-    // Apply the roll mode to adjust message visibility
-    ChatMessage.applyRollMode(chatData, game.settings.get("core", "rollMode"));
+  // Apply the roll mode to adjust message visibility
+  ChatMessage.applyRollMode(chatData, game.settings.get("core", "rollMode"));
 
-    // Create the Chat Message or return its data
-    return ChatMessage.create(chatData)
+  // Create the Chat Message or return its data
+  return ChatMessage.create(chatData)
 }
 
 async function updateChildren() {
-    let data = this.data.token.toObject()
-    let tokArr = Array.from(game.scenes.active.data.tokens)
-    let updateArr = tokArr.filter(i => i.data.actorId === this.id)
-    let updates = updateArr.map(i =>  (Object.assign({_id: i.id}, data)))
-    new Dialog({
-        title: game.i18n.localize("sidebar-context.updateChildrenTitle"),
-        content: game.i18n.localize("sidebar-context.updateChildrenContent"),
-        buttons: {
-            one: {
-                label: game.i18n.localize("Yes"),
-                callback: () => {
-                    game.scenes.active.updateEmbeddedDocuments("Token", updates)
-                }
-            }
+  let data = this.data.token.toObject()
+  let tokArr = Array.from(game.scenes.active.data.tokens)
+  let updateArr = tokArr.filter(i => i.data.actorId === this.id)
+  let updates = updateArr.map(i => (Object.assign({ _id: i.id }, data)))
+  new Dialog({
+    title: game.i18n.localize("sidebar-context.updateChildrenTitle"),
+    content: game.i18n.localize("sidebar-context.updateChildrenContent"),
+    buttons: {
+      one: {
+        label: game.i18n.localize("Yes"),
+        callback: () => {
+          game.scenes.active.updateEmbeddedDocuments("Token", updates)
         }
-    }).render(true)
+      }
+    }
+  }).render(true)
 
 }
 
 async function resetDoors(isCurrentScene, id) {
+  let updates = []
   if (isCurrentScene) {
-    await canvas
-      .walls?.doors.filter((item) => item.data.ds == 1)
-      .forEach((item) => item.update({ ds: 0 }, {}));
+    canvas
+      .walls?.doors.filter((item) => item.data.ds === 1)
+      .forEach((item) => updates.push({ _id: item.id, ds: 0 }))
+    await canvas.scene.updateEmbeddedDocuments("Wall", updates)
   } else {
     if (id) {
-      await game
-        .scenes?.get(id)
-        ?.data.walls.filter((item) => item.data.door != 0)
-        .forEach((x) => (x.data.ds = 0));
+      let scene = game.scenes?.get(id)
+      scene
+        .data.walls.filter((item) => item.data.ds === 1)
+        .forEach((x) => updates.push({ _id: x.id, ds : 0 }))
+      await scene.updateEmbeddedDocuments("Wall", updates)
     }
   }
   ui.notifications?.info(`Doors have been shut.`);
@@ -286,8 +289,8 @@ function setNavigationForAllScenes(folder, navOn) {
   const folderObject = game.folders.get(folder) || game.folders.getName(folder);
 
   const updates = game.scenes
-      .filter((scene) => scene.data.folder === folderObject.id)
-      .map((scene) => ({ _id: scene.id, navigation: navOn }));
+    .filter((scene) => scene.data.folder === folderObject.id)
+    .map((scene) => ({ _id: scene.id, navigation: navOn }));
 
-  return Scene.update(updates);
+  return Scene.updateDocuments(updates);
 }
