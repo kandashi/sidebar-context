d8c30d8bbd185e2aec0cbf43d98bdfb5ff555124
p4535992 <tentimarco0@gmail.com>
2023-03-01 22:12:33 +0100

    ready to test

---
+++ src/scripts/settings.ts
@@ -0,0 +1,10 @@
+export const registerSettings = function (): void {
+	// game.settings.register(CONSTANTS.MODULE_NAME, "applyOnATEItem", {
+	// 	name: i18n(`${CONSTANTS.MODULE_NAME}.setting.applyOnATEItem.name`),
+	// 	hint: i18n(`${CONSTANTS.MODULE_NAME}.setting.applyOnATEItem.hint`),
+	// 	scope: "world",
+	// 	config: true,
+	// 	type: Boolean,
+	// 	default: true,
+	// });
+};
